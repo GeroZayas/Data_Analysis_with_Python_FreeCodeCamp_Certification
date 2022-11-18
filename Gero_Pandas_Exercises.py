d37845eed7d46c46a358a6ec9882228fa4ebfcf1
GeroZayas <gerozayas@gmail.com>
2022-11-18 18:23:40 +0100

    Update

--- Gero_Pandas_Exercises.py
+++ Gero_Pandas_Exercises.py
@@ -1,77 +1,22 @@
+# Import pandas package
 import pandas as pd
-import numpy as np
 
-# X = ["A", "B", "C"]
-# print(X, type(X))
+# Define a dictionary containing Students data
+data = {
+    "Name": ["Jai", "Princi", "Gaurav", "Anuj"],
+    "Height": [5.1, 6.2, 5.1, 5.2],
+    "Qualification": ["Msc", "MA", "Msc", "Msc"],
+}
 
-# Y = pd.Series(X)
-# print(Y, type(Y))  # different type
+# Convert the dictionary into DataFrame
+df = pd.DataFrame(data)
 
-# Y.name = "My letters"
+# Declare a list that is to be converted into a column
+address = ['Delhi', 'Bangalore', 'Chennai', 'Patna']
 
-# print(Y)
+# Using 'Address' as the column name
+# and equating it to the list
+df['Address'] = address
 
-# print(Y.values)
-
-names_series = pd.Series(["Elisa", "Gero", "Mar", "Enrique", "Cristina"])
-
-print(names_series)
-
-# CREATE INDEXES:
-indexes = ["Mother", "The programmer", "Girlfriend", "Father", "Stepmother"]
-
-# PUT INDEXES TO SERIES:
-names_series.index = indexes
-
-# print(
-#     f"""
-# {names_series}
-
-# """
-# )
-
-# SHOW THE FIRST ELEMENT:
-
-print(names_series[0])  # Elisa
-print(names_series.iloc[0])  # Elisa
-print(names_series["Mother"])  # Elisa
-
-# SHOW THE LAST ELEMENT:
-
-print(names_series[-1])  # Cristina
-print(names_series.iloc[-1])  # Cristina
-print(names_series["Stepmother"])  # Cristina
-
-
-# Separator ------------------------------
-print("-" * 20)
-
-# SHOW ALL MIDDLE ELEMENTS:
-
-print(names_series[1:-1])
-
-
-# Separator ------------------------------
-print("-" * 20)
-
-# SHOW ELEMENTS IN REVERSE POSITION:
-
-# print(names_series[::-1])
-
-
-# Separator ------------------------------
-print("-" * 20)
-
-names_series = names_series[::-1]
-print(names_series)
-
-
-# Separator ------------------------------
-print("-" * 20)
-
-names_series += " [Great Person]"
-print(names_series)
-
-
-# Separator ------------------------------
-print("-" * 20)
+# Observe the result
+print(df)
